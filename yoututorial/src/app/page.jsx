50d59a50375bb3e948ae8d9a65dac5e823cfd49a
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-06-12 10:33:20 +0300

    linear gradient text application

--- yoututorial/src/app/page.jsx
+++ yoututorial/src/app/page.jsx
@@ -4,10 +4,17 @@ import Hero from 'public/hero.png';
 
 export default function Home() {
   return (
-    <div>
-      <Image
-        src={Hero}
-        alt='' className={styles.img}></Image>
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>Better design for your digital products.</h1>
+        <p className={styles.desc}>
+          Turning your idea into Reality. We bring together the teams from the global tech industry.
+        </p>
+        <button className={styles.button}>See out works</button>
+      </div>
+      <div className={styles.item}>
+        <Image src={Hero} alt='' className={styles.img}></Image>
+      </div>
     </div>
   )
 }
