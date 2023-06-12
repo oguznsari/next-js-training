fa7158b940c94aba96c801be14664c7e06147047
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-06-12 09:11:02 +0300

    added module.css support like styled components, use client for interactions

--- yoututorial/src/app/blog/[id]/page.jsx
+++ yoututorial/src/app/blog/[id]/page.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import styles from "./page.module.css";
 
 const Post = () => {
     return (
-        <div>
+        <div className={styles.container}>
             Blog Post page
         </div>
     )
--- yoututorial/src/components/navbar/Navbar.jsx
+++ yoututorial/src/components/navbar/Navbar.jsx
@@ -1,5 +1,7 @@
+"use client"
 import Link from 'next/link'
 import React from 'react'
+import styles from "./page.module.css";
 
 const links = [
     {
@@ -36,14 +38,22 @@ const links = [
 
 const Navbar = () => {
     return (
-        <div>
-            <Link href="/">lamamia</Link >
-            <div>
+        <div className={styles.container} >
+            <Link href="/" className={styles.logo}>lamamia</Link >
+            <div className={styles.links}>
                 {links.map(link => (
-                    <Link key={link.id} href={link.url}>
+                    <Link key={link.id} href={link.url} className={styles.link}>
                         {link.title}
                     </Link>
                 ))}
+                <button
+                    className={styles.logout}
+                    onClick={() => {
+                        console.log("logged out.")
+                    }}
+                >
+                    Logout
+                </button>
             </div>
         </div>
     )
