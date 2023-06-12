31de528fb5713fc42a5f58c05b43250bb79587dc
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-06-12 19:11:03 +0300

    blog

--- yoututorial/src/app/blog/[id]/page.jsx
+++ yoututorial/src/app/blog/[id]/page.jsx
@@ -1,10 +1,49 @@
 import React from "react";
 import styles from "./page.module.css";
+import Image from "next/image";
 
 const Post = () => {
     return (
         <div className={styles.container}>
-            Blog Post page
+            <div className={styles.top}>
+                <div className={styles.info}>
+                    <h1 className={styles.title}>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, est? Distinctio earum repudiandae, eius aperiam ducimus nobis, reiciendis quaerat quo deserunt aut magnam quidem sit veniam voluptates, sequi enim sint.
+                    </h1>
+                    <p className={styles.desc}>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus quasi, maxime dolorem quas unde neque, totam sequi ducimus, sed veniam modi assumenda ullam quidem vitae corporis. Eum voluptas corporis quibusdam.
+                    </p>
+                    <div className={styles.author}>
+                        <Image
+                            src="https://images.pexels.com/photos/6090285/pexels-photo-6090285.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+                            alt=""
+                            width={40}
+                            height={40}
+                            className={styles.avatar}
+                        />
+                        <span className={styles.username}>John Doe</span>
+                    </div>
+                </div>
+                <div className={styles.imageContainer}>
+                    <Image
+                        src="https://images.pexels.com/photos/6090285/pexels-photo-6090285.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+                        alt=""
+                        fill={true}
+                        className={styles.image}
+                    />
+                </div>
+            </div>
+
+            <div className={styles.content}>
+                <p className={styles.text}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Error ipsam hic quas id ratione! Quaerat ducimus maxime dolores. Voluptatem debitis eveniet adipisci minus eos aliquam quae aut iusto dolore officiis?
+                    <br /><br />
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis non quae, delectus error saepe quaerat mollitia architecto esse ad. Dicta sit minus eaque est consectetur minima atque at facere dolore?
+                    <br /><br />
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex, porro corrupti. Optio, adipisci. Ratione voluptatem cum iure quod numquam alias voluptates eligendi! Amet quidem eos cum eum molestiae fugiat doloremque?
+                </p>
+            </div>
+
         </div>
     )
 }
--- yoututorial/src/app/blog/page.jsx
+++ yoututorial/src/app/blog/page.jsx
@@ -1,10 +1,64 @@
 import React from "react";
 import styles from "./page.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
 const Blog = () => {
     return (
-        <div className={styles.container}>
-            Blog page
+        <div className={styles.mainContainer}>
+            <Link href="/blog/testId" className={styles.container}>
+                <div className={styles.imageContainer}>
+                    <Image
+                        src="https://images.pexels.com/photos/6090285/pexels-photo-6090285.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+                        alt=""
+                        width={400}
+                        height={250}
+                        className={styles.image}
+                    />
+                </div>
+                <div className={styles.content}>
+                    <h1 className={styles.title}>Test</h1>
+                    <p className={styles.content}>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente dolor, totam asperiores vel quo eaque omnis quos iste minima et nihil. Ducimus quam doloribus itaque, ipsam illo accusantium numquam est.
+                    </p>
+                </div>
+            </Link>
+
+            <Link href="/blog/testId" className={styles.container}>
+                <div className={styles.imageContainer}>
+                    <Image
+                        src="https://images.pexels.com/photos/6090285/pexels-photo-6090285.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+                        alt=""
+                        width={400}
+                        height={250}
+                        className={styles.image}
+                    />
+                </div>
+                <div className={styles.content}>
+                    <h1 className={styles.title}>Test</h1>
+                    <p className={styles.content}>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente dolor, totam asperiores vel quo eaque omnis quos iste minima et nihil. Ducimus quam doloribus itaque, ipsam illo accusantium numquam est.
+                    </p>
+                </div>
+            </Link>
+
+            <Link href="/blog/testId" className={styles.container}>
+                <div className={styles.imageContainer}>
+                    <Image
+                        src="https://images.pexels.com/photos/6090285/pexels-photo-6090285.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+                        alt=""
+                        width={400}
+                        height={250}
+                        className={styles.image}
+                    />
+                </div>
+                <div className={styles.content}>
+                    <h1 className={styles.title}>Test</h1>
+                    <p className={styles.content}>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente dolor, totam asperiores vel quo eaque omnis quos iste minima et nihil. Ducimus quam doloribus itaque, ipsam illo accusantium numquam est.
+                    </p>
+                </div>
+            </Link>
         </div>
     )
 }
