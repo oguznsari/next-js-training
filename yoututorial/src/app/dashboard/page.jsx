e11442d0eca1c1f6e85ccb54cc6717516cbc6579
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-06-12 23:21:16 +0300

    fetch data on client side with swr (built in error & loading features)

--- yoututorial/src/app/dashboard/page.jsx
+++ yoututorial/src/app/dashboard/page.jsx
@@ -1,32 +1,41 @@
 "use client"
 import React, { useEffect, useState } from "react";
 import styles from "./page.module.css";
+import useSWR from 'swr';
+
 
 const Dashboard = () => {
-    const [data, setData] = useState([]);
-    const [error, setError] = useState(false);
-    const [isLoading, setIsLoading] = useState(false);
+    // const [data, setData] = useState([]);
+    // const [error, setError] = useState(false);
+    // const [isLoading, setIsLoading] = useState(false);
 
-    useEffect(() => {
-        const getData = async () => {
-            setIsLoading(true);
-            const res = await fetch(
-                `https://jsonplaceholder.typicode.com/posts/`,
-                { cache: 'no-store' }
-            );
+    // useEffect(() => {
+    //     const getData = async () => {
+    //         setIsLoading(true);
+    //         const res = await fetch(
+    //             `https://jsonplaceholder.typicode.com/posts/`,
+    //             { cache: 'no-store' }
+    //         );
 
-            if (!res.ok) {
-                setError(true);
-                return notFound();
-            }
+    //         if (!res.ok) {
+    //             setError(true);
+    //             return notFound();
+    //         }
 
-            const data = await res.json();
-            setData(data);
-            setIsLoading(false);
-        }
+    //         const data = await res.json();
+    //         setData(data);
+    //         setIsLoading(false);
+    //     }
 
-        getData();
-    }, []);
+    //     getData();
+    // }, []);
+
+    const fetcher = (...args) => fetch(...args).then(res => res.json());
+
+    const { data, error, isLoading } = useSWR(
+        'https://jsonplaceholder.typicode.com/posts/',
+        fetcher
+    );
 
     console.log({ data });
 
