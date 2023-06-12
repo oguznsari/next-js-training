c774263f71374ed951c1ef700cea1b7473900652
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-06-12 22:54:45 +0300

    data fetching

--- yoututorial/src/app/blog/page.jsx
+++ yoututorial/src/app/blog/page.jsx
@@ -3,62 +3,40 @@ import styles from "./page.module.css";
 import Link from "next/link";
 import Image from "next/image";
 
-const Blog = () => {
+async function getData() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+
+    if (!res.ok) {
+        throw new Error('Failed to fetch data')
+    }
+
+    return res.json()
+}
+
+const Blog = async () => {
+    const data = await getData();
+
     return (
         <div className={styles.mainContainer}>
-            <Link href="/blog/testId" className={styles.container}>
-                <div className={styles.imageContainer}>
-                    <Image
-                        src="https://images.pexels.com/photos/6090285/pexels-photo-6090285.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
-                        alt=""
-                        width={400}
-                        height={250}
-                        className={styles.image}
-                    />
-                </div>
-                <div className={styles.content}>
-                    <h1 className={styles.title}>Test</h1>
-                    <p className={styles.content}>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente dolor, totam asperiores vel quo eaque omnis quos iste minima et nihil. Ducimus quam doloribus itaque, ipsam illo accusantium numquam est.
-                    </p>
-                </div>
-            </Link>
-
-            <Link href="/blog/testId" className={styles.container}>
-                <div className={styles.imageContainer}>
-                    <Image
-                        src="https://images.pexels.com/photos/6090285/pexels-photo-6090285.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
-                        alt=""
-                        width={400}
-                        height={250}
-                        className={styles.image}
-                    />
-                </div>
-                <div className={styles.content}>
-                    <h1 className={styles.title}>Test</h1>
-                    <p className={styles.content}>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente dolor, totam asperiores vel quo eaque omnis quos iste minima et nihil. Ducimus quam doloribus itaque, ipsam illo accusantium numquam est.
-                    </p>
-                </div>
-            </Link>
-
-            <Link href="/blog/testId" className={styles.container}>
-                <div className={styles.imageContainer}>
-                    <Image
-                        src="https://images.pexels.com/photos/6090285/pexels-photo-6090285.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
-                        alt=""
-                        width={400}
-                        height={250}
-                        className={styles.image}
-                    />
-                </div>
-                <div className={styles.content}>
-                    <h1 className={styles.title}>Test</h1>
-                    <p className={styles.content}>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente dolor, totam asperiores vel quo eaque omnis quos iste minima et nihil. Ducimus quam doloribus itaque, ipsam illo accusantium numquam est.
-                    </p>
-                </div>
-            </Link>
+            {data.map(item => (
+                <Link href="/blog/testId" className={styles.container} key={item.id} >
+                    <div className={styles.imageContainer}>
+                        <Image
+                            src="https://images.pexels.com/photos/6090285/pexels-photo-6090285.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+                            alt=""
+                            width={400}
+                            height={250}
+                            className={styles.image}
+                        />
+                    </div>
+                    <div className={styles.content}>
+                        <h1 className={styles.title}>{item.title}</h1>
+                        <p className={styles.content}>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente dolor, totam asperiores vel quo eaque omnis quos iste minima et nihil. Ducimus quam doloribus itaque, ipsam illo accusantium numquam est.
+                        </p>
+                    </div>
+                </Link>
+            ))}
         </div>
     )
 }
