9ec299797d030839121d34f1608aae81f9c9b326
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-06-12 11:07:11 +0300

    about page || new button component

--- yoututorial/src/app/about/page.jsx
+++ yoututorial/src/app/about/page.jsx
@@ -1,10 +1,32 @@
 import React from "react";
 import styles from "./page.module.css";
+import Image from "next/image";
+import Button from "@/components/Button/Button";
 
 const About = () => {
     return (
         <div className={styles.container}>
-            About page
+            <div className={styles.imgContainer}>
+                <Image
+                    src="https://images.pexels.com/photos/14579361/pexels-photo-14579361.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+                    alt="" fill={true} className={styles.img}>
+                </Image>
+                <div className={styles.imgText}>
+                    <h1 className={styles.imgTitle}>Digital Storytellers</h1>
+                    <h2 className={styles.imgDescription}>Handcrafting award winning digital experiences</h2>
+                </div>
+            </div>
+            <div className={styles.textContainer}>
+                <div className={styles.item}>
+                    <h1 className={styles.title}>Who we are?</h1>
+                    <p className={styles.desc}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis, vero quod praesentium nam quia vitae magnam esse sapiente, officia ipsam consequuntur neque repellendus distinctio rerum cupiditate voluptates. Aliquid, distinctio et.</p>
+                </div>
+                <div className={styles.item}>
+                    <h1 className={styles.title}>What we do?</h1>
+                    <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate expedita ab veniam porro repudiandae sed blanditiis hic facilis aspernatur non officiis consequatur dicta, nostrum fuga accusantium eos vel esse nam.</p>
+                    <Button url="contact" text="Contact" />
+                </div>
+            </div>
         </div>
     )
 }
--- yoututorial/src/app/page.jsx
+++ yoututorial/src/app/page.jsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import styles from './page.module.css'
 import Hero from 'public/hero.png';
+import Button from '@/components/Button/Button';
 
 export default function Home() {
   return (
@@ -10,7 +11,7 @@ export default function Home() {
         <p className={styles.desc}>
           Turning your idea into Reality. We bring together the teams from the global tech industry.
         </p>
-        <button className={styles.button}>See out works</button>
+        <Button url="/portfolio" text="See out works" />
       </div>
       <div className={styles.item}>
         <Image src={Hero} alt='' className={styles.img}></Image>
