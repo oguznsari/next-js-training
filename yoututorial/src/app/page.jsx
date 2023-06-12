21676c6e4c720eb058edcb16b689d58c12624c9a
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-06-12 09:45:22 +0300

    Next images -> external domains setup, import images, use public ones with width & height

--- yoututorial/src/app/page.jsx
+++ yoututorial/src/app/page.jsx
@@ -1,9 +1,13 @@
+import Image from 'next/image';
 import styles from './page.module.css'
+import Hero from 'public/hero.png';
 
 export default function Home() {
   return (
     <div>
-      Home
+      <Image
+        src={Hero}
+        alt='' className={styles.img}></Image>
     </div>
   )
 }
