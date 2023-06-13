0273c52d46f369a9634ca6fe7452d9d7812f35e9
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-06-13 11:28:39 +0300

    Oauth login with google | nextAuth

--- yoututorial/src/app/dashboard/page.jsx
+++ yoututorial/src/app/dashboard/page.jsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from "react";
 import styles from "./page.module.css";
 import useSWR from 'swr';
+import { useSession } from "next-auth/react";
 
 
 const Dashboard = () => {
@@ -30,6 +31,9 @@ const Dashboard = () => {
     //     getData();
     // }, []);
 
+    const session = useSession();
+    console.log({ session });
+
     const fetcher = (...args) => fetch(...args).then(res => res.json());
 
     const { data, error, isLoading } = useSWR(
@@ -37,7 +41,7 @@ const Dashboard = () => {
         fetcher
     );
 
-    console.log({ data });
+    // console.log({ data });
 
     return (
         <div>
