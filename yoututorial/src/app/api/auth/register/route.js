3a516f3378523f0fa2902496d6c21527f5cbef8d
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-06-13 12:19:21 +0300

    register and login with credentials || email & password

---
+++ yoututorial/src/app/api/auth/register/route.js
@@ -0,0 +1,35 @@
+import User from "@/models/User";
+import connect from "@/utils/db";
+import { StatusCodes } from "http-status-codes";
+import { NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+
+export const POST = async (request) => {
+    const { name, email, password } = await request.json();
+
+    await connect();
+
+    const hashedPassword = await bcrypt.hash(
+        password,
+        10
+    );
+
+    const newUser = new User({
+        name,
+        email,
+        password: hashedPassword
+    })
+
+    try {
+        await newUser.save();
+        return new NextResponse(
+            "User has been created",
+            { status: StatusCodes.CREATED }
+        )
+    } catch (error) {
+        return new NextResponse(
+            error.message,
+            { status: StatusCodes.INTERNAL_SERVER_ERROR }
+        )
+    }
+}
